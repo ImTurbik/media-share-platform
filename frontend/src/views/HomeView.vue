3e0087fbfdad9c5f3bcb89677b84ca71a22d753f
ImTurbik <alexshuxtin@yandex.ru>
2026-07-07 19:32:21 +0300

    fix: add refresh feed button

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -159,9 +159,15 @@ onMounted(() => {
 
 <template>
   <div class="max-w-xl mx-auto space-y-6">
-    <h3 class="text-xl font-black text-white tracking-tight px-1">
+    <!-- <h3 class="text-xl font-black text-white tracking-tight px-1">
       А это лента публикаций :>
-    </h3>
+    </h3> -->
+
+    <div class="flex justify-center">
+      <button @click="fetchPosts" class="inline-flex items-center justify-center rounded-lg bg-zinc-800 px-4 py-2 text-sm font-semibold text-zinc-100 shadow-md shadow-black/20 transition-colors hover:bg-zinc-700 active:bg-zinc-600 ">
+        Обновить ленту
+      </button>
+    </div>
 
     <div 
       v-for="post in posts" 
